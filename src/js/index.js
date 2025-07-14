9c947623c784ddc32528d2fa4c79e573b111b5f7
feliph <feliphrenaud@gmail.com>
2025-07-14 16:01:56 -0300

    Commit de estruturação de projeto finalizado proximo passo inserindo logica

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
-console.log(`a oooo mundao`);
+const container = document.querySelector("#input")
+const inputSubmit = document.querySelector("#submit")
+
+
+//Events
+
+function generateQrCode() {
+  const inputValue = container.value
+  
+  if (!inputValue) {
+    console.log("é obrigatorio inserir um valor ");
+    return 
+  }
+  
+  console.log("teste");
+  console.log(inputValue);
+
+  
+}
+inputSubmit.addEventListener("click", (e) => {
+  e.preventDefault()
+  generateQrCode()
+  
+})

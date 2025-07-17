d38dc7fa248dff20a0959cc7e0ad943aba002cd7
feliph <feliphrenaud@gmail.com>
2025-07-17 16:13:03 -0300

    Commit de finalização do projeto onde foram atualizados a logica do programa  e estilização do css

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,33 @@
-const container = document.querySelector("#input")
-const inputSubmit = document.querySelector("#submit")
-
+const inputText = document.querySelector("#input")// input de entrada
+const inputSubmit = document.querySelector("#submit")//antigo input de submit trocado por btn 
+const qrCodeImg = document.querySelector("#qr-img")// imagem de qrcode gerado
+const container = document.querySelector(".container")
 
 //Events
 
 function generateQrCode() {
-  const inputValue = container.value
+  const inputValue = inputText.value
   
   if (!inputValue) {
     console.log("é obrigatorio inserir um valor ");
     return 
   }
+  inputSubmit.innerText = "Gerando QRcode..."
+  qrCodeImg.src = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputValue}`
   
+
+  qrCodeImg.addEventListener("load", () => {
+    container.classList.add("active")
+    inputSubmit.innerText = "QRcode Gerado"
+  })
+
   console.log("teste");
   console.log(inputValue);
 
   
 }
-inputSubmit.addEventListener("click", (e) => {
-  e.preventDefault()
+inputSubmit.addEventListener("click", () => {
+ 
   generateQrCode()
   
 })
